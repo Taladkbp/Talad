21f2292df118d36859b7e1ddad320fcb4e2b7820
taladkbp <talad.kbp@gmail.com>
2023-05-05 08:26:19 +0700

    Added ReactNativePaper

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { registerRootComponent } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from './navigations/BottomTabNavigator';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <BottomTabNavigator/>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <BottomTabNavigator/>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
