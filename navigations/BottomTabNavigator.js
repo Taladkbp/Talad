18083cb3156759e83b8d6f0aac0f655f1be4194a
taladkbp <talad.kbp@gmail.com>
2023-05-05 09:01:10 +0700

    Created PhoneVerification and ProfileSetup Screen

--- navigations/BottomTabNavigator.js
+++ navigations/BottomTabNavigator.js
@@ -6,6 +6,8 @@ import LivestreamScreen from '../screens/LivestreamScreen'
 import ShopScreen from '../screens/ShopScreen'
 import AnalyticsScreen from '../screens/AnalyticsScreen'
 import LoginScreen from '../screens/LoginScreen'
+import ProfileSetupScreen from '../screens/ProfileSetupScreen'
+import PhoneVerificationScreen from '../screens/PhoneVerificationScreen'
 
 import { HomeIcon as HomeIconSolid,
     VideoCameraIcon as VideoCameraIconSolid,
@@ -40,7 +42,11 @@ const BottomTabNavigator = () => {
             iconName = focused ? <ChartBarSquareIconSolid color={color} size={size}/> : <ChartBarSquareIconOutline color={color} size={size}/>;
           } else if (route.name === 'Login') {
             iconName = focused ? <ChartBarSquareIconSolid color={color} size={size}/> : <ChartBarSquareIconOutline color={color} size={size}/>;
-          }
+          } else if (route.name === 'PhoneVerification') {
+            iconName = focused ? <ChartBarSquareIconSolid color={color} size={size}/> : <ChartBarSquareIconOutline color={color} size={size}/>;
+          } else if (route.name === 'ProfileSetup') {
+            iconName = focused ? <ChartBarSquareIconSolid color={color} size={size}/> : <ChartBarSquareIconOutline color={color} size={size}/>;
+          } 
           
           
           return iconName
@@ -54,6 +60,11 @@ const BottomTabNavigator = () => {
       <Tab.Screen name='Shop' component={ShopScreen}/>
       <Tab.Screen name='Analytics' component={AnalyticsScreen}/>
       <Tab.Screen name='Login' component={LoginScreen}/>
+      <Tab.Screen name='PhoneVerification' component={PhoneVerificationScreen}/>
+      <Tab.Screen name='ProfileSetup' component={ProfileSetupScreen}/>
+
+
+      
     </Tab.Navigator>
   )
 }
---
+++ screens/ProfileSetupScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text } from 'react-native'
+
+const ProfileSetupScreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>Test</Text>
+    </SafeAreaView>
+  )
+}
+
+export default ProfileSetupScreen
