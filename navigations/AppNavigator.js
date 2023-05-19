cbe302be027e99bec03556916014ed930bb6657c
taladkbp <talad.kbp@gmail.com>
2023-05-19 09:00:50 +0700

    Added bottom Tab for user icon\

--- navigations/AppNavigator.js
+++ navigations/AppNavigator.js
@@ -1,17 +1,50 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react'
 import UserTabNavigator from './UserTabNavigator';
 import AuthNavigator from './AuthNavigator';
-
+import { createContext, useCallback, useMemo, useRef } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+import BottomSheetContext from '../components/BottomSheetContext';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
+  const bottomSheetRef = useRef(null);
+  const snapPoints = useMemo(() => ['25%', '50%', '100%'], []);
+
+  const handleOpenSheet = useCallback(() => {
+    bottomSheetRef.current.snapToIndex(1)
+  }, []);
+
+  const renderContent = () => {
+    return (
+      <View>
+        <Text>Testing</Text>
+      </View>
+    );
+  }
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='UserApp' component={UserTabNavigator}/>
-      <Stack.Screen name='Authentication' component={AuthNavigator}/>
-    </Stack.Navigator>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetContext.Provider value={handleOpenSheet}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen 
+              name='UserApp' 
+              component={UserTabNavigator}
+              />
+          <Stack.Screen name='Authentication' component={AuthNavigator}/>
+        </Stack.Navigator>
+        <BottomSheet
+          ref={bottomSheetRef}
+          index={-1}
+          snapPoints={snapPoints}
+          >
+          {renderContent()}
+        </BottomSheet>
+      </BottomSheetContext.Provider>
+    </GestureHandlerRootView>
   )
 }
 
--- navigations/TopTabNavigator.js
+++ navigations/TopTabNavigator.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import AccountScreen from '../screens/AccountScreen';
+import React, { useCallback, useContext, useMemo, useRef } from 'react'
 import { UserCircleIcon } from 'react-native-heroicons/solid';
 import { BuildingStorefrontIcon } from 'react-native-heroicons/outline';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Colors } from '../src/theme/colors.js'
-import { View } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
+import BottomSheetContext from '../components/BottomSheetContext.js';
 
 
 const TopTabNavigator = () => {
+  const openSheet = useContext(BottomSheetContext)
   return (
     <SafeAreaView>
       <View className='flex-row p-2 justify-between'>
-
-        <UserCircleIcon color={Colors.primary} size={34}/>
-          <Button
-            mode='contained'
-            icon={({ color, size }) => <BuildingStorefrontIcon color={'#ffffff'} size={22} />}
-            contentStyle={{ height: 34, justifyContent: 'center' }}
-            labelStyle={{ lineHeight: 17, fontSize: 16 }}
-          
-          >
-            Create
-          </Button>
+        <TouchableOpacity onPress={openSheet}>
+          <UserCircleIcon color={Colors.primary} size={34}/>
+        </TouchableOpacity>
+        <Button
+          mode='contained'
+          icon={({ color, size }) => <BuildingStorefrontIcon color={'#ffffff'} size={22} />}
+          contentStyle={{ height: 34, justifyContent: 'center' }}
+          labelStyle={{ lineHeight: 17, fontSize: 16 }}
+        
+        >
+          Create
+        </Button>
       </View>
     </SafeAreaView>
   )
