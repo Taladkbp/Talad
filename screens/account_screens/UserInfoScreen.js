3a0eaee7552b53d01021a0ad1cdc3f5c39b5cda7
taladkbp <talad.kbp@gmail.com>
2023-05-22 08:57:30 +0700

    Added birthday calendar for profile

--- screens/account_screens/UserInfoScreen.js
+++ screens/account_screens/UserInfoScreen.js
@@ -3,56 +3,78 @@ import { Text, TextInput } from 'react-native-paper'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Colors } from '../../src/theme/colors'
 import { View } from 'react-native'
+import BackButton from '../../components/BackButton'
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const UserInfoScreen = () => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [birthday, setBirthday] = useState("");
-  const [location, setLocation] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
 
+  const showDatePicker = () => {
+    setShow(true);
+  };
 
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const formatDate = (date) => {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  }
   return (
     <SafeAreaView className='flex-1'>
+      <BackButton/>
       <View className='flex-row justify-center my-10'>
-        <Text> Create a new account</Text>
+        <Text className='text-3xl font-bold'> Set up your profile</Text>
       </View>
       <View className='flex-1 px-5 mt-5'>
-        <Text>First name: </Text>
         <TextInput
           value={firstName}
           onChangeText={setFirstName}
-          className='h-10 border border-gray-300 '
+          className='h-10 border border-gray-300 mb-4'
           style={{ backgroundColor: Colors.surface}}
+          placeholder='First Name'
+          placeholderTextColor={Colors.onPlaceholder}
         />
-        <Text>Last name: </Text>
         <TextInput
           value={lastName}
           onChangeText={setLastName}
-          className='h-10 border border-gray-300 '
+          className='h-10 border border-gray-300 mb-4'
           style={{ backgroundColor: Colors.surface}}
+          placeholder='Last Name'
+          placeholderTextColor={Colors.onPlaceholder}
         />
-        <Text>Phone number: </Text>
         <TextInput
           value={phoneNumber}
           onChangeText={setPhoneNumber}
-          className='h-10 border border-gray-300 '
+          className='h-10 border border-gray-300 mb-4'
           style={{ backgroundColor: Colors.surface}}
+          placeholder='Phone Number'
+          placeholderTextColor={Colors.onPlaceholder}
         />
-        <Text>Birthday: </Text>
-        <TextInput
-          value={birthday}
-          onChangeText={setBirthday}
-          className='h-10 border border-gray-300 '
+        <TouchableOpacity 
+          className='h-10 border border-gray-300 mb-4 justify-center'
           style={{ backgroundColor: Colors.surface}}
-        />
-        <Text>Location: </Text>
-        <TextInput
-          value={location}
-          onChangeText={setLocation}
-          className='h-10 border border-gray-300 '
-          style={{ backgroundColor: Colors.surface}}
-        />
+          onPress={showDatePicker}> 
+          <Text className='pl-4 text-base'>Birthday: {formatDate(date)}</Text>
+        </TouchableOpacity>
+        {show && (
+          <DateTimePicker
+            testID='dateTimePicker'
+            value={date}
+            mode='date'
+            display='default'
+            onChange={onChange}
+            maximumDate={new Date()}
+          />
+        )}
       </View>
     </SafeAreaView>
   )
--- src/theme/colors.js
+++ src/theme/colors.js
@@ -4,12 +4,14 @@ export const Colors = {
   background: '#F5F5F5', // Light gray for the background
   surface: '#FFFFFF', // White for cards and surfaces
   text: '#424242', // Dark gray for the text
+  lighttext: '#808080', // Light gray for text
   error: '#f44336', // Red color for errors
   onPrimary: '#FFFFFF', // White text on primary color
   onAccent: '#000000', // Black text on accent color
   onBackground: '#424242', // Dark gray text on background color
   onSurface: '#424242', // Dark gray text on surface color
   onUserIcon: '#D3D2E2',
+  onPlaceholder: '#808080',
 };
 
 export const activeTabColor = '#4CAF50';
