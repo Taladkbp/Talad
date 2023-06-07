19f965f82efa36311c6f0437338554776795b054
taladkbp <talad.kbp@gmail.com>
2023-06-07 08:17:37 +0700

    Added Maps and GeoLocation in UserInfoScreen

--- screens/account_screens/UserInfoScreen.js
+++ screens/account_screens/UserInfoScreen.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
-import { Text, TextInput } from 'react-native-paper'
+import { ActivityIndicator, Text, TextInput } from 'react-native-paper'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Colors } from '../../src/theme/colors'
-import { View } from 'react-native'
+import { KeyboardAvoidingView, ScrollView, View } from 'react-native'
 import BackButton from '../../components/BackButton'
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useEffect } from 'react'
 import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps'
 
 
 const UserInfoScreen = () => {
@@ -16,7 +17,7 @@ const UserInfoScreen = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [date, setDate] = useState(null);
   const [show, setShow] = useState(false);
-  const [address, setAddress] = useState('');
+  const [location, setLocation] = useState('');
 
   const showDatePicker = () => {
     setShow(true);
@@ -51,8 +52,13 @@ const UserInfoScreen = () => {
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      console.log(location)
+      let locationResult = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: locationResult.coords.latitude,
+        longitude: locationResult.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
 
     })();
   }, []);
@@ -60,52 +66,82 @@ const UserInfoScreen = () => {
   
   return (
     <SafeAreaView className='flex-1'>
-      <BackButton/>
-      <View className='flex-row justify-center my-10'>
-        <Text className='text-3xl font-bold'> Set up your profile</Text>
-      </View>
-      <View className='flex-1 px-5 mt-5'>
-        <TextInput
-          value={firstName}
-          onChangeText={setFirstName}
-          className='h-10 border border-gray-300 mb-4'
-          style={{ backgroundColor: Colors.surface}}
-          placeholder='First Name'
-          placeholderTextColor={Colors.onPlaceholder}
-        />
-        <TextInput
-          value={lastName}
-          onChangeText={setLastName}
-          className='h-10 border border-gray-300 mb-4'
-          style={{ backgroundColor: Colors.surface}}
-          placeholder='Last Name'
-          placeholderTextColor={Colors.onPlaceholder}
-        />
-        <TextInput
-          value={phoneNumber}
-          onChangeText={setPhoneNumber}
-          className='h-10 border border-gray-300 mb-4'
-          style={{ backgroundColor: Colors.surface}}
-          placeholder='Phone Number'
-          placeholderTextColor={Colors.onPlaceholder}
-        />
-        <TouchableOpacity 
-          className='h-10 border border-gray-300 mb-4 justify-center'
-          style={{ backgroundColor: Colors.surface}}
-          onPress={showDatePicker}> 
-          <Text className='pl-4 text-base'>Birthday: {date ? formatDate(date): 'Please enter your birthday'}</Text>
-        </TouchableOpacity>
-        {show && (
-          <DateTimePicker
-            testID='dateTimePicker'
-            value={date || new Date()}
-            mode='date'
-            display='default'
-            onChange={onChangeDate}
-            maximumDate={new Date()}
-          />
-        )}
-      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"} 
+        className='flex-1'
+      >
+        <ScrollView>
+          <BackButton/>
+          <View className='flex-row justify-center my-10'>
+            <Text className='text-3xl font-bold'> Set up your profile</Text>
+          </View>
+          <View className='flex-1 px-5 mt-5'>
+            <TextInput
+              value={firstName}
+              onChangeText={setFirstName}
+              className='h-10 border border-gray-300 mb-4'
+              style={{ backgroundColor: Colors.surface}}
+              placeholder='First Name'
+              placeholderTextColor={Colors.onPlaceholder}
+            />
+            <TextInput
+              value={lastName}
+              onChangeText={setLastName}
+              className='h-10 border border-gray-300 mb-4'
+              style={{ backgroundColor: Colors.surface}}
+              placeholder='Last Name'
+              placeholderTextColor={Colors.onPlaceholder}
+            />
+            <TextInput
+              value={phoneNumber}
+              onChangeText={setPhoneNumber}
+              className='h-10 border border-gray-300 mb-4'
+              style={{ backgroundColor: Colors.surface}}
+              placeholder='Phone Number'
+              placeholderTextColor={Colors.onPlaceholder}
+            />
+            <TouchableOpacity 
+              className='h-10 border border-gray-300 mb-4 justify-center'
+              style={{ backgroundColor: Colors.surface}}
+              onPress={showDatePicker}> 
+              <Text className='pl-4 text-base'>Birthday: {date ? formatDate(date): 'Please enter your birthday'}</Text>
+            </TouchableOpacity>
+            {show && (
+              <DateTimePicker
+                testID='dateTimePicker'
+                value={date || new Date()}
+                mode='date'
+                display='default'
+                onChange={onChangeDate}
+                maximumDate={new Date()}
+              />
+            )}
+          </View>
+          <View className='flex-1'>
+            <View className='mt-2 mb-2'>
+              <Text className='text-lg font-bold'>Delivery Address:</Text>
+            </View>
+            <View className='shadow-md rounded-lg bg-white overflow-hidden'>
+                {location ? (
+                  <MapView
+                    style={{ width: '100%', height: 250 }}
+                    region={location}
+                    onRegionChangeComplete={setLocation}
+                    zoomEnabled={true}
+                    zoomControlEnabled={true}
+                  > 
+                    <Marker coordinate={location}/>
+                  </MapView>
+                ) : (
+                  <View className='h-60 justify-center items-center'>
+                    <ActivityIndicator size="large" color={Colors.primary}/>
+                    <Text className='pt-3'>Loading map...</Text>
+                  </View>
+                )}
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
