dd98df18bea9cecc83dc35febcbbed65e6768ab1
taladkbp <talad.kbp@gmail.com>
2023-06-13 22:00:03 +0700

    Added camera with requestion for permission button

--- src/screens/LivestreamScreen.js
+++ src/screens/LivestreamScreen.js
@@ -1,32 +1,42 @@
 import { Camera, CameraType } from 'expo-camera';
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native'
-import { View } from 'react-native'
-import { Text } from 'react-native-paper';
+import { View, TouchableOpacity } from 'react-native'
+import { Button, Text } from 'react-native-paper';
 
 const LivestreamScreen = () => {
   const [type, setType] = useState(CameraType.back);
-  const [hasPermission, setHasPermission] = Camera.useCameraPermissions();
+  const [permission, requestPermission] = Camera.useCameraPermissions();
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync()
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View/>
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
   }
 
-  if (hasPermission === false) {
-    return <Text>Please give access to Camera</Text>
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View className='flex-1 justify-center'>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back))
   }
 
   return (
-    <SafeAreaView className='flex-1'>
-      <Camera className='flex-1' type={type}/>
-    </SafeAreaView>
+    <View className='flex-1 justify-center'>
+      <Camera className='flex-1' type={type}>
+        <View className='flex-1 flex-row bg-transparent m-16'>
+          <TouchableOpacity className='flex-1 self-end items-center' onPress={toggleCameraType}>
+            <Text className='text-2xl font-bold text-white'>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
   )
 }
 
