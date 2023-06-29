c1b922ab98efeeecd70f2557015608c0fb5f47a5
taladkbp <talad.kbp@gmail.com>
2023-06-29 08:34:09 +0700

    Able to close login modal on livestream screen

--- src/screens/login_screens/LoginModal.js
+++ src/screens/login_screens/LoginModal.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Modal, Text } from 'react-native-paper'
 import { View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { XCircleIcon } from 'react-native-heroicons/solid'
+import { Colors } from '../../theme/colors'
 
 
 const LoginModal = ({visible, setVisible}) => {
@@ -12,8 +15,18 @@ const LoginModal = ({visible, setVisible}) => {
       visible={visible}
       onRequestClose={() => setVisible(false)}
     >
-      <View className='flex-1 justify-center items-center'>
-        <Text> Testing</Text>
+      <View className='flex-1 justify-center items-center bg-gray-500 bg-opacity-50 px-4'>
+        <View className='bg-white rounded-lg p-6 w-full h-56'>
+          <View className='absolute top-2 right-2'>
+            <TouchableOpacity
+              onPress={() => {console.log('on press is called')}}
+            >
+            <XCircleIcon size={30} color={Colors.primary}/>
+          </TouchableOpacity>
+          </View>
+          <Text className='text-center text-2xl font-bold my-2'>Login Required</Text>
+          <Text className='text-center mb-4'>Please login to continue</Text>
+        </View>
       </View>
     </Modal>
   )
