0dd4d80cfd7dcf0f45052e0b8a99a33c44c4b9d3
taladkbp <talad.kbp@gmail.com>
2023-07-03 08:47:39 +0700

    Added go live function but still not working, has Provide valid app Id error with facebook api

--- src/screens/LivestreamScreen.js
+++ src/screens/LivestreamScreen.js
@@ -8,15 +8,18 @@ import { Button, Text } from 'react-native-paper';
 import { AuthContext } from '../auth/AuthProvider';
 import { Colors } from '../theme/colors';
 import LoginModal from './login_screens/LoginModal';
+import axios from 'axios';
 
 const LivestreamScreen = () => {
+  const { user } = useContext(AuthContext);
   const [type, setType] = useState(CameraType.back);
   const [cameraPermission, setCameraPermission] = useState(null);
   const [recording, setRecording] = useState(false);
   const [microphonePermission, setMicrophonePermission] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
-  const { user } = useContext(AuthContext);
+  const [liveVideoId, setLiveVideoId] = useState(null);
 
+  // Requesting Mic and Camera Permission
   const requestCameraPermission = async () => {
     console.log("Requesting camera permission...");
     const { status } = await Camera.requestCameraPermissionsAsync();
@@ -34,6 +37,7 @@ const LivestreamScreen = () => {
     requestMicrophonePermission();
   }, []);
 
+  // Checking for Mic and Camera Permission
   // Bring back the || microphonePermission === null
   if (cameraPermission === null) {
     // Camera permissions are still loading
@@ -63,23 +67,52 @@ const LivestreamScreen = () => {
   }
 
   const startLiveStream = async () => {
-    let data;
+    let facebookId;
 
     if (!user || !user.providerData.some(provider => provider.providerId === 'facebook.com')) {
       setModalVisible(true)
     } else {
-      data = await AccessToken.getCurrentAccessToken();
-      console.log(data)
+      user.providerData.forEach(provider => {
+        if (provider.providerId === 'facebook.com') {
+          facebookId = provider.uid;
+        }
+      });
+      const data = await AccessToken.getCurrentAccessToken();
+      // console.log(data)
+
+      if (data) {
+        axios.post(`https://graph.facebook.com/${facebookId}/live_videos`, {
+          access_token: data.accessToken,
+          status: 'LIVE_NOW'
+        })
+        .then(response => {
+          console.log('Live video ID:', response.data.id);
+          setLiveVideoId(response.data.id);
+        })
+        .catch(error => {
+          console.log('Failed to start live video: ', error)
+        })
+      }
     }
-  }
+  };
+
+  const stopLiveStream = async () => {
+    if (!liveVideoId) return;
+
+    const data = await AccessToken.getCurrentAccessToken();
+
+    if (data) {
+      axios.post(`https://graph.facebook.com/${liveVideoId}`)
+    }
+  };
 
   const toggleCameraType = () => {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back))
-  }
+  };
 
   const toggleMicrophone = () => { // Add this function
     setRecording(current => !current)
-  }
+  };
 
   return (
     <SafeAreaView className='flex-1'>
@@ -94,6 +127,14 @@ const LivestreamScreen = () => {
                 icon={({ color, size }) => <Ionicons name='radio' color={'#ffffff'} size={22} />}
                 >
                 Go Live
+              </Button>
+              <Button 
+                buttonColor={Colors.primary} 
+                mode='contained'
+                onPress={startLiveStream}
+                icon={({ color, size }) => <Ionicons name='radio' color={'#ffffff'} size={22} />}
+                >
+                Stop
               </Button>
             </TouchableOpacity>
             <View className='flex-row mx-5'>
